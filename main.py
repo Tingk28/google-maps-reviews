b5cd389638baff45acb41b0b2ed218b40d85573a
alex33883840 <71116547+alex33883840@users.noreply.github.com>
2021-02-20 13:26:05 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from selenium.webdriver.common.by import By
 import json
 import time
 import os.path
+import sys
+'''
+readme:python main.py file_name data_name sort_index fliter
+sort_index: 1-4 相關/新/高/低
+e.g. python main.py store_list.txt 億進寢具.json 3 4
+
+'''
+
 
 def load_comment_section(link):  # 載入留言區並寫入分店名稱
     global store, number
@@ -11,9 +19,21 @@ def load_comment_section(link):  # 載入留言區並寫入分店名稱
     try:
         store = driver.find_element_by_tag_name("h1").text+'.json'
         number_str = driver.find_element(By.CSS_SELECTOR, ".reviews-tap-area > span > .widget-pane-link").text
-
         number = int(number_str.split(" ")[0].replace(',','')) # 修改留言總數
         driver.find_element(By.CSS_SELECTOR, ".reviews-tap-area > span > .widget-pane-link").click()
+        if int(sys.argv[3]) > 0:  # sort index
+            #sys.argv[3] = int(sys.argv[3])+25738
+            #element = "[vet='" + str(sys.argv[3]) + "']"
+            try:
+                driver.find_element_by_css_selector("[aria-label='排序評論']").click()
+                print("click0")
+                driver.find_element_by_css_selector("[vet='25740']").click()
+                #driver.find_element_by_css_selector("[aria-label='排序評論']").click()
+                #driver.find_element_by_css_selector(element).click()
+                time.sleep(4)
+            except Exception as e:
+                print(e)
+                print("無法排序")
     except Exception as e:
         print(e)
         print(store[:-5]+" 沒有評論")
@@ -27,44 +47,60 @@ def get_comment():
 
 def parsing(dataset):
     comment_dict = {}
-    for i in comment_list:
+    for i in dataset:
         comment = []
         comment_id = i.find_element_by_class_name("section-review-title").text
         star = i.find_element_by_class_name("section-review-stars").get_attribute("aria-label")
         text = i.find_element_by_class_name("section-review-text").text
+        date = i.find_element_by_class_name("section-review-publish-date").text
         comment.append(int(star[1]))
         comment.append(text)
+        comment.append(date)
         comment_dict[comment_id] = comment
     return comment_dict
-
 options = webdriver.ChromeOptions()
-#options.add_argument("--headless")
-options.add_argument("--window-size=%s" % "1920,1080")
+# options.add_argument("--headless")
+options.add_argument("--window-size=%s" % "720,780")
 driver = webdriver.Chrome(r'C:\Users\alex9\PycharmProjects\pythonProject\fb_hw_helper\fb_login\chromedriver.exe', options =options)
 driver.implicitly_wait(15)  #隱性等待，至多15秒
-store_list = open("store_list.txt", "r+").read().split("\n")
-
+store_list = open(sys.argv[1], "r+").read().split("\n")
+comment_list = []
 
 for url in store_list:
     store = ''  #店名(json檔名)
     number = 0
     load_comment_section(url)
-    if number>0:
-        comment_list = []
+    print(store)
+    print(number)
+    if number > 0:
         pre_number = 0
         time_stamp = time.time()
-        while len(comment_list)<number:
-            comment_list = get_comment()
-            if len(comment_list)!=pre_number:
-                pre_number=len(comment_list)
+        temp_comment_list = []
+        while len(temp_comment_list) < number:
+            temp_comment_list = get_comment()
+            if len(temp_comment_list) != pre_number:
+                pre_number = len(temp_comment_list)
                 time_stamp = time.time()
-            elif time.time()-time_stamp>90:
+                if int(sys.argv[3]) > 0:
+                    if sys.argv[3] == 2: # 新
+                        print("opppppppps")
+                    elif sys.argv[3] == 3 and int(comment_list[-1].find_element_by_class_name("section-review-stars").get_attribute("aria-label")[1])<sys.argv[4]: # 高
+                        break
+                    elif sys.argv[3] == 4 and int(comment_list[-1].find_element_by_class_name("section-review-stars").get_attribute("aria-label")[1])>sys.argv[4]: # 低
+                        break
+            elif time.time()-time_stamp > 90:
                 print("time_out")
                 break
             try:
                 driver.find_element_by_class_name('section-loading').click()  # 向下滾動
-            except:
-                print("沒有更多留言")
+            except Exception as e:
+                print(e)
                 break
-        with open(os.path.join(os.getcwd(),'data',store), 'w', encoding='utf-8') as f:
-            json.dump(parsing(comment_list),f,ensure_ascii=False)
+        print("沒有更多留言")
+        comment_list += temp_comment_list
+
+
+# if ".json" not in sys.argv[2]:
+    sys.argv[2] = store[:12].replace("/","")+".json"
+with open(os.path.join(os.getcwd(),'data',sys.argv[2]), 'w', encoding='utf-8') as f:
+    json.dump(parsing(comment_list),f,ensure_ascii=False)
